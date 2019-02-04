8c421ce7aaa756ca9794c44c65a958926c06b42c
Chun <cw1753@nyu.edu>
2019-02-03 23:27:34 -0500

    Fixed request timeout problem with next() in Adjust CORS

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
         return res.status(200).json({});
     }
+    next();
 })
 
 //Initialize routes
