63ca06299226d1dde8afa62d05da51c3a5bc544c
Chun <cw1753@nyu.edu>
2019-02-03 23:47:37 -0500

    Testing some requires

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passportSetup = require('./config/passport-setup');
+const passport = require('passport');
 const dotenv = require('dotenv').config();
 
 
